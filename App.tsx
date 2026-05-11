ac7bffef3f1c3f7e1c0f7ad7661e6a336efb1f19
Nam Yooseong <nys8765@naver.com>
2026-05-11 15:10:38 +0900

    React 적용하기

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { emojis } from "./emoji";
+import { format } from "date-fns";
+
+const App = () => {
+  const [selectedEmoji, setSelectedEmoji] = React.useState(emojis[0]);
+
+  const showRandomEmoji = () => {
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    setSelectedEmoji(emojis[randomIndex]);
+  };
+
+  return (
+    <div className="container">
+      <img src="./assets/logo.svg" alt="Logo" className="logo"></img>
+      <h1>Emoji of the Day</h1>
+      <div className="date-display">{format(new Date(), "MMMM d, yyyy")}</div>
+      <div className="emoji-container">
+        <div className="emoji">{selectedEmoji.icon}</div>
+        <div className="emoji-name">{selectedEmoji.name}</div>
+        <button onClick={showRandomEmoji}>See other emoji</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./main.ts", // 어떤 파일을 진입점으로 번들링할지
+  entry: "./main.tsx", // 어떤 파일을 진입점으로 번들링할지
   output: {
     filename: "bundle.js", // 번들로 만들어질 파일 이름
     path: path.resolve(__dirname, "dist"), // 번들 파일이 어디에 저장될지
@@ -11,13 +11,24 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/, // .ts 파일들은
-        use: "ts-loader", // ts-loader를 거쳐 처리된다.
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        ],
         exclude: /node_modules/, // 외부 모듈은 제외한다.
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"], // 파일을 import할 때 확장자를 생략할 수 있다. TS, JS를 혼용하는 프로젝트에서 설정해두면 좋다.
+    extensions: [".tsx", ".ts", ".js"], // 파일을 import할 때 확장자를 생략할 수 있다. .tsx 확장자, TS, JS를 혼용하는 프로젝트에서 설정해두면 좋다.
   },
 };
